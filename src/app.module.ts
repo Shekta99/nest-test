0a3243fc265f475a62010e18e43495e3020ec432
Sergio Marentes <smarentes@osnetpr.com>
2024-03-12 08:57:22 -0500

    adding example controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { ExampleController } from './example.controller';
 import { AppService } from './app.service';
+import { ExampleService } from './example.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController,ExampleController],
+  providers: [AppService, ExampleService],
 })
 export class AppModule {}
